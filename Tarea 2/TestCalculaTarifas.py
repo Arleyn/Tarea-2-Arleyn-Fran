13c1a5dc61897e8530d1e6bd8a1c91316449083c
FranciscoJavier <FranciscoJavier@FranEscritorio>
2015-04-22 21:25:04 -0430

    Segundo Commit (Prueba basica)

--- Tarea 2/TestCalculaTarifas.py
+++ Tarea 2/TestCalculaTarifas.py
@@ -5,4 +5,16 @@ Created on 22/4/2015
          Francisco Sucre  10-10717
 '''
 
+import unittest
+
+class Test(unittest.TestCase):
+
+    def testCalcularPrecios(self):
+        pass
+    
+    
+if __name__ == "__main__":
+#import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
+
 
