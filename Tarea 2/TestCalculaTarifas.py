c977d8d060dc8e77c81ec3b3c1af7d78ac02dfd0
user.Arleyn <arleyn.g.2712@hotmail.com>
2015-04-22 22:23:07 -0430

    Cuarto commit

--- Tarea 2/TestCalculaTarifas.py
+++ Tarea 2/TestCalculaTarifas.py
@@ -20,7 +20,6 @@ class Test(unittest.TestCase):
                                     datetime.datetime(2015,4,27,0,0,0)];
         self.assertEqual(0,CalculaTarifas.calcularPrecio(tarifa,tiempo))
         
-        
     # Prueba Frontera lo minimo son 15 dias en un dia de semana
     
     def test15MinutosSemana(self):
@@ -29,7 +28,47 @@ class Test(unittest.TestCase):
         tiempo = [datetime.datetime(2015,4,21,16,0,0),
                                     datetime.datetime(2015,4,21,16,15,0)];
         
-        self.assertEqual(5,CalculaTarifas.calcularPrecio(tarifa,tiempo))        
+        self.assertEqual(5,CalculaTarifas.calcularPrecio(tarifa,tiempo))
+        
+    # Prueba Frontera lo minimo son 15 dias en un fin de semana
+        
+    def test15MinutosFinSemana(self):
+        
+        tarifa = CalculaTarifas.Tarifa(5,10);
+        tiempo = [datetime.datetime(2015,4,25,16,0,0),
+                                    datetime.datetime(2015,4,25,16,15,0)];
+        
+        self.assertEqual(10,CalculaTarifas.calcularPrecio(tarifa,tiempo))
+        
+    # Prueba 5 dias exacto de Lunes a Viernes
+        
+    def test5DiasSemanaExacto(self):
+        
+        tarifa = CalculaTarifas.Tarifa(5,10);
+        tiempo = [datetime.datetime(2015,4,20,0,0,0),
+                                    datetime.datetime(2015,4,25,0,0,0)];
+        
+        self.assertEqual(600,CalculaTarifas.calcularPrecio(tarifa,tiempo))
+        
+    # Prueba 2 dias exactos de Sabado a Domingo
+        
+    def test2DiasFinSemanaExacto(self):
+        
+        tarifa = CalculaTarifas.Tarifa(5,10);
+        tiempo = [datetime.datetime(2015,4,25,0,0,0),
+                                    datetime.datetime(2015,4,27,0,0,0)];
+        
+        self.assertEqual(480,CalculaTarifas.calcularPrecio(tarifa,tiempo))
+        
+    # Prueba de 1 hora y 1 minuto, probar que se cobra 2 horas
+
+    def test1hora1minuto(self):
+        
+        tarifa = CalculaTarifas.Tarifa(5,10);
+        tiempo = [datetime.datetime(2015,4,21,16,0,0),
+                                    datetime.datetime(2015,4,21,17,1,0)];
+        
+        self.assertEqual(10,CalculaTarifas.calcularPrecio(tarifa,tiempo))        
     
 if __name__ == "__main__":
 #import sys;sys.argv = ['', 'Test.testName']
